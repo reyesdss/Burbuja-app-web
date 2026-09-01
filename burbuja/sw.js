297618ced8bc082be16cf2a54753863141b31a2d
reyesdssj <reyesdssj@gmail.com>
2026-09-01 04:23:14 -0600

    Delete burbuja directory

--- burbuja/sw.js
+++
@@ -1,3 +0,0 @@
-const OLD_PREFIX="burbuja-shell";
-self.addEventListener("install",()=>self.skipWaiting());
-self.addEventListener("activate",event=>{event.waitUntil((async()=>{try{const keys=await caches.keys();await Promise.all(keys.filter(k=>k.startsWith(OLD_PREFIX)).map(k=>caches.delete(k)));await self.registration.unregister();const clientsList=await self.clients.matchAll({type:"window"});clientsList.forEach(c=>c.navigate(c.url));}catch{}})())});
